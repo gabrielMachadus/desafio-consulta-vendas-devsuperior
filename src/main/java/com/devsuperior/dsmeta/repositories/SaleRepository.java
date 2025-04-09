5e997db47bac1d08a141450c2cfab9950ab3e278
MachadoGabriel97 <6249824@aluno.uniasselvi.com.br>
2025-04-09 16:13:09 -0300

    Inclusão de Queqys JPQL no Repository

--- src/main/java/com/devsuperior/dsmeta/repositories/SaleRepository.java
+++ src/main/java/com/devsuperior/dsmeta/repositories/SaleRepository.java
@@ -1,9 +1,30 @@
 package com.devsuperior.dsmeta.repositories;
 
+import com.devsuperior.dsmeta.dto.SaleReportDTO;
+import com.devsuperior.dsmeta.dto.SaleSummaryDTO;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import com.devsuperior.dsmeta.entities.Sale;
+import org.springframework.data.jpa.repository.Query;
+
+import java.time.LocalDate;
+import java.util.List;
 
 public interface SaleRepository extends JpaRepository<Sale, Long> {
+
+    @Query("SELECT new com.devsuperior.dsmeta.dto.SaleReportDTO(obj.id, obj.date, obj.amount, obj.seller.name) "
+            + "FROM Sale obj "
+            + "WHERE obj.date BETWEEN :min AND :max "
+            + "AND UPPER(obj.seller.name) LIKE UPPER(CONCAT('%', :name, '%'))")
+    Page<SaleReportDTO> searchReport(LocalDate min, LocalDate max, String name, Pageable pageable);
+
+
+    @Query("SELECT new com.devsuperior.dsmeta.dto.SaleSummaryDTO(obj.seller.name, SUM(obj.amount)) "
+            + "FROM Sale obj "
+            + "WHERE obj.date BETWEEN :min AND :max "
+            + "GROUP BY obj.seller.name")
+    List<SaleSummaryDTO> searchSummary(LocalDate min, LocalDate max);
 
 }
