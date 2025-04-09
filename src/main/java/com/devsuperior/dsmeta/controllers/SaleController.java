b592f020fb5984a449685c9c64d035e0014752ae
MachadoGabriel97 <6249824@aluno.uniasselvi.com.br>
2025-04-09 16:17:34 -0300

    Inclusão dos endpoint no controller SaleController

--- src/main/java/com/devsuperior/dsmeta/controllers/SaleController.java
+++ src/main/java/com/devsuperior/dsmeta/controllers/SaleController.java
@@ -1,14 +1,17 @@
 package com.devsuperior.dsmeta.controllers;
 
+import com.devsuperior.dsmeta.dto.SaleReportDTO;
+import com.devsuperior.dsmeta.dto.SaleSummaryDTO;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import com.devsuperior.dsmeta.dto.SaleMinDTO;
 import com.devsuperior.dsmeta.services.SaleService;
+
+import java.util.List;
 
 @RestController
 @RequestMapping(value = "/sales")
@@ -23,15 +26,21 @@ public class SaleController {
 		return ResponseEntity.ok(dto);
 	}
 
-	@GetMapping(value = "/report")
-	public ResponseEntity<?> getReport() {
-		// TODO
-		return null;
+	@GetMapping("/report")
+	public Page<SaleReportDTO> report(
+			@RequestParam(required = false) String minDate,
+			@RequestParam(required = false) String maxDate,
+			@RequestParam(required = false) String name,
+			Pageable pageable) {
+
+		return service.getReport(minDate, maxDate, name, pageable);
 	}
 
-	@GetMapping(value = "/summary")
-	public ResponseEntity<?> getSummary() {
-		// TODO
-		return null;
+	@GetMapping("/summary")
+	public List<SaleSummaryDTO> summary(
+			@RequestParam(required = false) String minDate,
+			@RequestParam(required = false) String maxDate) {
+
+		return service.getSummary(minDate, maxDate);
 	}
 }
